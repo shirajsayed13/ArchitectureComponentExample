4ea55ac3aa3dde3017e3db2af025fb8f9865875f
Shiraj Sayed <shirajsayed13@gmail.com>
2019-05-25 13:08:37 +0530

    Initial commit and setup room database for app

---
+++ app/src/main/java/com/example/architecturecomponentexample/data/NoteDatabase.java
@@ -0,0 +1,26 @@
+package com.example.architecturecomponentexample.data;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Note.class}, version = 1, exportSchema = false)
+public abstract class NoteDatabase extends RoomDatabase {
+    private static final String DATABASE_NAME = "note_database";
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class, DATABASE_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
