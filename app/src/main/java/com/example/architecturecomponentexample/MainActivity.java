897c534c6006e0ebe55696ec1972fb660870f836
Shiraj Sayed <shirajsayed13@gmail.com>
2019-05-25 17:50:01 +0530

    Add layout files, recycler adapter and setup with activity

--- app/src/main/java/com/example/architecturecomponentexample/MainActivity.java
+++ app/src/main/java/com/example/architecturecomponentexample/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.architecturecomponentexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.architecturecomponentexample.data.Note;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ProgressBar progressBar = findViewById(R.id.progress_bar);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
+        NoteViewModel noteViewModel = ViewModelProviders.of(this)
+                .get(NoteViewModel.class);
+
+        noteViewModel.getAllNotes().observe(this,
+                new Observer<List<Note>>() {
+                    @Override
+                    public void onChanged(List<Note> notes) {
+                        //Toast.makeText(MainActivity.this, "onChanged Called", Toast.LENGTH_LONG).show();
+                        adapter.setNotes(notes);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/architecturecomponentexample/NoteAdapter.java
@@ -0,0 +1,63 @@
+package com.example.architecturecomponentexample;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.architecturecomponentexample.data.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private List<Note> notes;
+
+    public NoteAdapter() {
+        notes = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list, parent, false);
+        return new NoteHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        Note currentNote = notes.get(position);
+        holder.mTitle.setText(currentNote.getTitle());
+        holder.mDescription.setText(currentNote.getDescription());
+        holder.mPriority.setText(String.valueOf(currentNote.getPriority()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteHolder extends RecyclerView.ViewHolder {
+        private AppCompatTextView mTitle;
+        private AppCompatTextView mDescription;
+        private AppCompatTextView mPriority;
+
+        public NoteHolder(@NonNull View itemView) {
+            super(itemView);
+            mTitle = itemView.findViewById(R.id.title);
+            mDescription = itemView.findViewById(R.id.description);
+            mPriority = itemView.findViewById(R.id.priority);
+        }
+    }
+
+}
